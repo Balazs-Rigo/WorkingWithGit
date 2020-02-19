ae5be5f8a31a19c2fd7fb00ee041e68264f3d343
Balázs Rigó <rigo.balazs86@gmail.com>
2020-02-19 04:55:39 +0100

    Add code files.

---
+++ SimpleConsole/BucketSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresMoshPart3
+{
+    public class BucketSort
+    {
+        public void Sort(int[] array, int numberOfBuckets)
+        {
+            QuickSort sort = new QuickSort();
+            var i = 0;
+            foreach (var bucket in CreateBuckets(array,numberOfBuckets))
+            {
+                bucket.Sort();
+                foreach (var item in bucket)
+                {
+                    array[i++] = item;
+                }
+
+            }            
+        }
+        private List<List<int>> CreateBuckets(int[] array, int numberOfBuckets)
+        {
+            List<List<int>> buckets = new List<List<int>>();
+            for (var i = 0; i < numberOfBuckets; i++)
+                buckets.Add(new List<int>());
+
+            foreach (var item in array)
+                buckets[item / numberOfBuckets].Add(item);
+
+            return buckets;
+        }
+    }
+}
